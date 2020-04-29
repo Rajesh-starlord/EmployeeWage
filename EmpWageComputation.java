683eaeaa85c7ab81255bfbb87cc8e887f17401ce
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-04-29 18:37:30 +0530

    starts with welcome message

---
+++ EmpWageComputation.java
@@ -0,0 +1,8 @@
+//start with welcome message
+
+class EmpWageComputation{
+   public static void main(String[] args){
+	//welcome message...
+    System.out.println("Welcome to Employee wage computation program");
+   }//main
+}//class
