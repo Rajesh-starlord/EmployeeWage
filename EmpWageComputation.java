4cd6a718f76075297bf07893267cca30e0642eaa
Rajesh-starlord <56430104+Rajesh-starlord@users.noreply.github.com>
2020-04-29 18:57:41 +0530

    Update EmpWageComputation.java

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,7 +5,7 @@ class EmpWageComputationUc2{
     int wage_per_hr=20;
     int full_day_hr=8;
     System.out.println("Welcome to Employee wage computation program");
-	double attCheck=Math.random();
+    double attCheck=Math.random();
     if(attCheck>0.5){
         System.out.println("Employee is present");
         System.out.println("daily_wage="+(wage_per_hr*full_day_hr))";
