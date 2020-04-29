fb048d47df0da1a458a8865c563dbbb218667564
Rajesh-starlord <56430104+Rajesh-starlord@users.noreply.github.com>
2020-04-29 19:17:01 +0530

    added uc-3

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,18 +1,24 @@
 //UC-3 add part time
 
 class EmpWageComputationUc3{
-   public static void main(String[] args){
-    int wage_per_hr=20;
-    int full_day_hr=8;
-    System.out.println("Welcome to Employee wage computation program");
-        double attCheck=Math.random();
-    if(attCheck>0.5){
-        System.out.println("Employee is present");
-        System.out.println("daily_wage="+(wage_per_hr*full_day_hr))";
-    }else{
-        System.out.println("Employee is absent");
-        System.out.println("daily_wage="+(wage_per_hr*0))";
-    }
+   public static void main(String[] args)throws Exception{
+      int wage_per_hr=20;
+      int full_day_hr=8;
+      int half_day_hr=4;
+      System.out.println("Welcome to Employee wage computation program");
+      double attCheck=Math.random();
+      if(attCheck>0.5){
+         double category=Math.random();
+         if (category>0.5){
+            System.out.println("Employee is present and is a full time");
+            System.out.println("daily_wage="+(wage_per_hr*full_day_hr));
+         }else{
+            System.out.println("Employee is present and is a part time");
+            System.out.println("daily_wage="+(wage_per_hr*half_day_hr));
+         }
+      }else{
+         System.out.println("Employee is absent");
+         System.out.println("daily_wage="+(wage_per_hr*0));
+      }
    }
 }
-
