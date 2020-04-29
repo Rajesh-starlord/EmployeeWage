5a5c7d2dfe3b8df6a532ed1e54621f5480c6208d
Rajesh-starlord <rajeshkanhar.rk10@gmail.com>
2020-04-29 18:55:29 +0530

    added UC-2

---
+++ EmpWageComputation.java
@@ -0,0 +1,17 @@
+//UC-2 Calculate daily wage
+
+class EmpWageComputationUc2{
+   public static void main(String[] args){
+    int wage_per_hr=20;
+    int full_day_hr=8;
+    System.out.println("Welcome to Employee wage computation program");
+	double attCheck=Math.random();
+    if(attCheck>0.5){
+        System.out.println("Employee is present");
+        System.out.println("daily_wage="+(wage_per_hr*full_day_hr))";
+    }else{
+        System.out.println("Employee is absent");
+        System.out.println("daily_wage="+(wage_per_hr*0))";
+    }
+   }
+}
